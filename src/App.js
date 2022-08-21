a9d3eee0b1f465bd91de8a6197dbff093fae3a44
islam alghoul <islamalghoul1997>
2022-08-21 23:41:42 +0300

    t1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React ,{Component} from "react";
 import Main  from "./Main";
-
-
+import Header from "./Header";
+import Footer from "./Footer";
 class App extends Component {
   render(){
       return(
   
         <div>
-        <Main/></div>
+          <Header/>
+        <Main/>
+        <Footer/>
+        </div>
    
           
       )
