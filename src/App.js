39aa9d4ca93561aba489819d6a5efe543d16efba
islam alghoul <islamalghoul1997>
2022-08-23 23:08:12 +0300

    lab3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React ,{Component} from "react";
-import Main  from "./Main";
+import React, { Component } from "react";
+import Main from "./Main";
 import Header from "./Header";
 import Footer from "./Footer";
+import data from "./data.json";
+import SelectedBeast from "./SelectedBeast"
+
+
+
 class App extends Component {
-  render(){
-      return(
   
-        <div>
-          <Header/>
-        <Main/>
-        <Footer/>
-        </div>
-   
-          
-      )
+  constructor() {
+    super()
+    this.state = {
+         showCard:""
+
+    }
+    
+
+}
+onClickHandler=()=>{
+  alert("hi")
+
+}
+
+  render() {
+ 
+    return (
+
+      <div>
+        <Header />
+        <Main Data={data} handel={this.onClickHandler}/>       
+        <Footer />
+      </div>
+
+
+    )
   }
-  
-  }
-  export default App;
+
+}
+export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,71 @@
-import React,{Component} from "react";
-import HornedBeast from "./HornedBeast";
-import data from "./data.json"
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 class Main extends Component {
-    render(){
-        return(
-<div> {data.map((i)=>{
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            numberOfVotes: "❤️"
+
+        }
+
+
+    }
+
    
-   return <div><HornedBeast id={i._id} imgUrl={i.image_url}title={i.title} description={i.description} keyword={i.keyword} horns={i.horns} /></div>
-})}
-            
-            
-           
-          
-            </div>   
+    clickHandler = () => {
+        this.setState(
+            {
+                numberOfVotes : this.state.numberOfVotes + "❤️"
+            }
         )
+        
     }
     
+
+
+    render() {    
+
+        return (
+           <div>
+             
+<Row xs={1} md={3} className="g-4">
+
+{this.props.Data.map((i) => {
+  
+    return (<Col>
+        <Card onClick={this.props.handel}  >
+    <Card.Img variant="top" src={i.image_url} onClick={this.clickHandler}/>
+        <Card.Body>
+        <Card.Title>{i.title}</Card.Title>
+                    <Card.Text>
+                        <p>{i.keyword}</p>
+                        <p>id:{i.id} </p>
+                        <p>{i.description}</p>
+                        <p>{i.horns}</p>
+                        <p>vote for your favorite beast {this.state.numberOfVotes}</p>
+                    </Card.Text >
+                </Card.Body>
+            </Card>
+            </Col>
+              )
+             
+       
+})}
+ </Row>
+              
+ </div>
+
+        );
     }
-    export default Main;
+
+}
+export default Main;
+
+
+
+
+
